da86e1dfd88ef523da899cbc3fa45422450882ee
Rishabh28111995 <109350653+Rishabh28111995@users.noreply.github.com>
2023-04-09 16:09:51 +0530

    Add files via upload

---
+++ Fibonacci.cs
@@ -0,0 +1,23 @@
+﻿namespace FibonaccciNumber;
+class Fibonacci
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the number of Fibonacci terms: ");
+        int numTerms = int.Parse(Console.ReadLine());
+
+        int a = 0, b = 1, c = 0;
+
+        Console.WriteLine("{0} {1}", a, b);
+
+        for( int i = 3; i <= numTerms; i++)
+        {
+            c = a + b;
+            Console.WriteLine(" {0}", c);
+            a = b;
+            b = c;
+        }
+        Console.WriteLine();
+    }
+}
+
