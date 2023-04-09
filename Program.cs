396ec59fe7d54aba94cdbcfb706654c38808fa5c
Rishabh28111995 <109350653+Rishabh28111995@users.noreply.github.com>
2023-04-09 17:25:50 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ReverseNumber;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int num;
+        int reversedNum = 0;
+        Console.WriteLine("Enter a number: ");
+        num = int.Parse(Console.ReadLine());
+
+        while (num > 0)
+        { 
+            int digit = num % 10;
+           reversedNum = reversedNum * 10 + digit;
+
+            num /= 10;
+        }
+        Console.WriteLine("Reversed number is: " + reversedNum);
+    }
+}
+
