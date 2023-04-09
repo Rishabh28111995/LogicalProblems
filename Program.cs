bfa06f3cc4dfdf7506c9990c536543a4b2bda28e
Rishabh28111995 <109350653+Rishabh28111995@users.noreply.github.com>
2023-04-09 16:53:56 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace PowerNumber;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int num = 28;
+        int sum = 0;
+
+        for (int i = 1; i <= num / 2; i++)
+        {
+            if (num % i == 0)
+            {
+                sum += i;
+            }
+        }
+
+        if (sum == num)
+        {
+            Console.WriteLine(num + " is Perfect number.");
+        }
+        else
+        {
+            Console.WriteLine(num + " is not Perfect number.");
+        }
+    }
+}
+
